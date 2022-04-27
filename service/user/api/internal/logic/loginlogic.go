cea8185918c596a0421288761450f4e8589f54b0
aqin97 <791041423@qq.com>
2022-04-27 15:13:29 +0800

    user-api用户登录逻辑完成

--- service/user/api/internal/logic/loginlogic.go
+++ service/user/api/internal/logic/loginlogic.go
@@ -2,9 +2,12 @@ package logic
 
 import (
 	"context"
+	"time"
 
+	"go-zero-mall/common/jwtx"
 	"go-zero-mall/service/user/api/internal/svc"
 	"go-zero-mall/service/user/api/internal/types"
+	"go-zero-mall/service/user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -28,6 +31,22 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 */
 func (l *LoginLogic) Login(req *types.LoginRequest) (resp *types.LoginResponse, err error) {
 	// todo: add your logic here and delete this line
+	res, err := l.svcCtx.UserRpc.Login(l.ctx, &user.LoginRequset{
+		Mobile:   req.Moblie,
+		Password: req.Password,
+	})
+	if err != nil {
+		return nil, err
+	}
+	now := time.Now().Unix()
+	accessExpire := l.svcCtx.Config.Auth.AccessExpire
+	accessToken, err := jwtx.GetToken(l.svcCtx.Config.Auth.AccessSecret, now, accessExpire, res.Id)
+	if err != nil {
+		return nil, err
+	}
 
-	return
+	return &types.LoginResponse{
+		AccessToken:  accessToken,
+		AccessExpire: accessExpire,
+	}, nil
 }
