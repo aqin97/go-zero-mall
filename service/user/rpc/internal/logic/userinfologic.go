0149988e8f2e76187526cc3f17058e28f97ec14b
aqin97 <791041423@qq.com>
2022-04-27 11:20:53 +0800

    rpc中添加用户信息逻辑

--- service/user/rpc/internal/logic/userinfologic.go
+++ service/user/rpc/internal/logic/userinfologic.go
@@ -3,10 +3,12 @@ package logic
 import (
 	"context"
 
+	"go-zero-mall/service/user/model"
 	"go-zero-mall/service/user/rpc/internal/svc"
 	"go-zero-mall/service/user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/status"
 )
 
 type UserInfoLogic struct {
@@ -25,6 +27,17 @@ func NewUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserInfo
 
 func (l *UserInfoLogic) UserInfo(in *user.UserInfoRequest) (*user.UserInfoResponse, error) {
 	// todo: add your logic here and delete this line
-
-	return &user.UserInfoResponse{}, nil
+	res, err := l.svcCtx.UserModel.FindOne(l.ctx, in.Id)
+	if err != nil {
+		if err == model.ErrNotFound {
+			return nil, status.Error(100, "用户不存在")
+		}
+		return nil, status.Error(500, err.Error())
+	}
+	return &user.UserInfoResponse{
+		Id:     res.Id,
+		Name:   res.Name,
+		Gender: res.Gender,
+		Mobile: res.Mobile,
+	}, nil
 }
