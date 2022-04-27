36a42e6a57879dda54f98f629f6244b2e440585d
aqin97 <791041423@qq.com>
2022-04-27 11:16:30 +0800

    rpc中添加用户登录逻辑

--- service/user/rpc/internal/logic/loginlogic.go
+++ service/user/rpc/internal/logic/loginlogic.go
@@ -3,10 +3,13 @@ package logic
 import (
 	"context"
 
+	"go-zero-mall/common/cryptx"
+	"go-zero-mall/service/user/model"
 	"go-zero-mall/service/user/rpc/internal/svc"
 	"go-zero-mall/service/user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/status"
 )
 
 type LoginLogic struct {
@@ -23,8 +26,28 @@ func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic 
 	}
 }
 
+/*
+用户登录逻辑login
+根据手机号查询用户是否注册过，如果是注册过的，判断密码，如果未注册，返回提示注册信息
+*/
 func (l *LoginLogic) Login(in *user.LoginRequset) (*user.LoginResponse, error) {
 	// todo: add your logic here and delete this line
+	res, err := l.svcCtx.UserModel.FindOneByMobile(l.ctx, in.Mobile)
+	if err != nil {
+		if err == model.ErrNotFound {
+			return nil, status.Error(100, "用户不存在")
+		}
+		return nil, status.Error(500, err.Error())
+	}
+	password := cryptx.PasswordEncrypt(l.svcCtx.Config.Salt, in.Password)
+	if password != res.Password {
+		return nil, status.Error(100, "密码错误")
+	}
 
-	return &user.LoginResponse{}, nil
+	return &user.LoginResponse{
+		Id:     res.Id,
+		Name:   res.Name,
+		Gender: res.Gender,
+		Mobile: res.Mobile,
+	}, nil
 }
