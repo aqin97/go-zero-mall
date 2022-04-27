03644cfe11ba25c8ca66b3bbfada9a2d070d3029
aqin97 <791041423@qq.com>
2022-04-27 10:37:35 +0800

    rpc中添加用户注册逻辑

--- service/user/rpc/internal/logic/registerlogic.go
+++ service/user/rpc/internal/logic/registerlogic.go
@@ -3,10 +3,13 @@ package logic
 import (
 	"context"
 
+	"go-zero-mall/common/cryptx"
+	"go-zero-mall/service/user/model"
 	"go-zero-mall/service/user/rpc/internal/svc"
 	"go-zero-mall/service/user/rpc/user"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/status"
 )
 
 type RegisterLogic struct {
@@ -23,8 +26,37 @@ func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Register
 	}
 }
 
+/*
+用户注册逻辑：
+判断手机号是否被注册，被注册返回提示信息，未注册将用户信息写入数据库
+*/
 func (l *RegisterLogic) Register(in *user.RegisterRequest) (*user.RegisterResponse, error) {
 	// todo: add your logic here and delete this line
-
-	return &user.RegisterResponse{}, nil
+	_, err := l.svcCtx.UserModel.FindOneByMobile(l.ctx, in.Mobile)
+	if err == nil {
+		return nil, status.Error(100, "该用户已经存在")
+	}
+	if err == model.ErrNotFound {
+		newUser := model.User{
+			Name:     in.Name,
+			Gender:   in.Gender,
+			Mobile:   in.Mobile,
+			Password: cryptx.PasswordEncrypt(l.svcCtx.Config.Salt, in.Password),
+		}
+		res, err := l.svcCtx.UserModel.Insert(l.ctx, &newUser)
+		if err != nil {
+			return nil, status.Error(500, err.Error())
+		}
+		newUser.Id, err = res.LastInsertId()
+		if err != nil {
+			return nil, status.Error(500, err.Error())
+		}
+		return &user.RegisterResponse{
+			Id:     newUser.Id,
+			Name:   newUser.Name,
+			Gender: newUser.Gender,
+			Mobile: newUser.Mobile,
+		}, nil
+	}
+	return nil, status.Error(500, err.Error())
 }
