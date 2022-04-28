e47a0f133ef4b37fd469ec39c621fcd2872d0201
aqin97 <791041423@qq.com>
2022-04-28 17:40:26 +0800

    成order-rpc订单更新逻辑

--- service/order/rpc/internal/logic/updatelogic.go
+++ service/order/rpc/internal/logic/updatelogic.go
@@ -3,10 +3,12 @@ package logic
 import (
 	"context"
 
+	"go-zero-mall/service/order/model"
 	"go-zero-mall/service/order/rpc/internal/svc"
 	"go-zero-mall/service/order/rpc/order"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/status"
 )
 
 type UpdateLogic struct {
@@ -25,6 +27,28 @@ func NewUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UpdateLogi
 
 func (l *UpdateLogic) Update(in *order.UpdateRequset) (*order.UpdateResponse, error) {
 	// todo: add your logic here and delete this line
-
+	res, err := l.svcCtx.OrderModel.FindOne(l.ctx, in.Id)
+	if err != nil {
+		if err == model.ErrNotFound {
+			return nil, status.Error(100, "该订单不存在")
+		}
+		return nil, status.Error(500, err.Error())
+	}
+	if in.Uid != 0 {
+		res.Uid = in.Uid
+	}
+	if in.Pid != 0 {
+		res.Pid = in.Pid
+	}
+	if in.Amount != 0 {
+		res.Amount = in.Amount
+	}
+	if in.Status != 0 {
+		res.Status = in.Status
+	}
+	err = l.svcCtx.OrderModel.Update(l.ctx, res)
+	if err != nil {
+		return nil, status.Error(500, err.Error())
+	}
 	return &order.UpdateResponse{}, nil
 }
