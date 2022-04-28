951b2e6599a781f537f673636f4d7d4fa068690f
aqin97 <791041423@qq.com>
2022-04-28 17:31:45 +0800

    完成order-rpc订单详情逻辑

--- service/order/rpc/internal/logic/detaillogic.go
+++ service/order/rpc/internal/logic/detaillogic.go
@@ -3,10 +3,12 @@ package logic
 import (
 	"context"
 
+	"go-zero-mall/service/order/model"
 	"go-zero-mall/service/order/rpc/internal/svc"
 	"go-zero-mall/service/order/rpc/order"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/status"
 )
 
 type DetailLogic struct {
@@ -25,6 +27,18 @@ func NewDetailLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DetailLogi
 
 func (l *DetailLogic) Detail(in *order.DetailRequest) (*order.DetailResponse, error) {
 	// todo: add your logic here and delete this line
-
-	return &order.DetailResponse{}, nil
+	res, err := l.svcCtx.OrderModel.FindOne(l.ctx, in.Id)
+	if err != nil {
+		if err == model.ErrNotFound {
+			return nil, status.Error(100, "该订单不存在")
+		}
+		return nil, status.Error(500, err.Error())
+	}
+	return &order.DetailResponse{
+		Id:     res.Id,
+		Uid:    res.Uid,
+		Pid:    res.Pid,
+		Amount: res.Amount,
+		Status: res.Status,
+	}, nil
 }
