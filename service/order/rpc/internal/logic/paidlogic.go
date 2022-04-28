2f84f6323f63e4269b6b7659ebfb363d5831f992
aqin97 <791041423@qq.com>
2022-04-28 21:44:29 +0800

    order-rpc完成支付逻辑，实际上就是修改订单中的状态

--- service/order/rpc/internal/logic/paidlogic.go
+++ service/order/rpc/internal/logic/paidlogic.go
@@ -3,10 +3,12 @@ package logic
 import (
 	"context"
 
+	"go-zero-mall/service/order/model"
 	"go-zero-mall/service/order/rpc/internal/svc"
 	"go-zero-mall/service/order/rpc/order"
 
 	"github.com/zeromicro/go-zero/core/logx"
+	"google.golang.org/grpc/status"
 )
 
 type PaidLogic struct {
@@ -25,6 +27,19 @@ func NewPaidLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PaidLogic {
 
 func (l *PaidLogic) Paid(in *order.PaidRequest) (*order.PaidResponse, error) {
 	// todo: add your logic here and delete this line
-
+	//查询订单是否存在
+	res, err := l.svcCtx.OrderModel.FindOne(l.ctx, in.Id)
+	if err != nil {
+		if err == model.ErrNotFound {
+			return nil, status.Error(100, "订单不存在")
+		}
+		return nil, status.Error(500, err.Error())
+	}
+	//修改订单状态， 1表示已支付
+	res.Status = 1
+	err = l.svcCtx.OrderModel.Update(l.ctx, res)
+	if err != nil {
+		return nil, status.Error(500, err.Error())
+	}
 	return &order.PaidResponse{}, nil
 }
